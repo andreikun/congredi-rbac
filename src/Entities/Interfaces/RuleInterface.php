6fef49d5c26f08e3ca63ffd36cd0df1da0d97d02
John van Terheijden <john@fastguide.eu>
2016-05-11 13:08:13 +0200

    Rule execute should be on user ID, not model.

--- src/Entities/Interfaces/RuleInterface.php
+++ src/Entities/Interfaces/RuleInterface.php
@@ -1,8 +1,6 @@
 <?php namespace Congredi\Rbac\Entities\Interfaces;
 
-use Illuminate\Database\Eloquent\Model;
-
 interface RuleInterface extends BaseInterface
 {
-	public function execute(Model $user, ItemInterface $item, $params);
+	public function execute($user, ItemInterface $item, $params);
 }
